faf89ed2a988d16d840d2e1423647dbd52b128a4
Edison Peng <edison.peng@caseware.com>
2023-04-03 11:45:17 -0400

    Second commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://edisondipeng:Mu4kIVbOYc2xNGGJ@edisoncluster.yilezwf.mongodb.net/todolistDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://edisondipeng:@edisoncluster.yilezwf.mongodb.net/todolistDB?retryWrites=true&w=majority");
 
 const itemsSchema = {
   name: String };
